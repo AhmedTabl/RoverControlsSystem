7ff9f4a6857aa2231006db635a4fd2d5a7afaaa3
AhmedTabl <63268015+AhmedTabl@users.noreply.github.com>
2023-10-03 19:31:05 -0400

    client sends and server recieves
    successfully sends arm and drive commands in the proper format

--- RoverControl/clientCode.py
+++ RoverControl/clientCode.py
@@ -33,8 +33,28 @@ D_DOWN = 12
 D_LEFT = 13
 D_RIGHT = 14
 
-TRIGGER_THRESHOLD = 0.1
+TRIGGER_THRESHOLD = 0.001
 button_states = [False] * controller.get_numbuttons()
+
+gantry = 0
+spin = 0
+wrist_right = 0
+wrist_left = 0
+cam1x = 90
+cam1y = 90
+cam2x = 90
+cam2y = 90
+shoulder = 0
+claw = 0
+
+isReverseCam1y = False
+isReverseCam2y = False
+isReverseCam1x = False
+isReverseCam2x = False
+isReverseOptions = False
+isReverseShare = False
+isReverseClaw = False
+
 
 
 try:
@@ -54,19 +74,21 @@ try:
                 right_pwm = int((right_y + 1) * 127.5)  # Scale to 0-255
 
                 # Create and send Drive Command packet
-                drive_command = f"D_{left_pwm}_{right_pwm}"
+                drive_command = f"D_{left_pwm}_{right_pwm}_{left_pwm}_{right_pwm}_{left_pwm}_{right_pwm}"
                 client_socket.send(drive_command.encode())
 
-                # Print the sent packet
-                #print("Sent Drive Command:", drive_command)
 
                 if event.axis == 4:  # L2
+
                     if event.value > TRIGGER_THRESHOLD:
-                        print(f"L2 trigger is pressed at value {event.value}")
+                        gantry = int((event.value + 1) * 63.5) #pwm 0 -> 127
+                        print(f"{gantry}")
+
 
                 elif event.axis == 5:  # R2
+
                     if event.value > TRIGGER_THRESHOLD:
-                        print(f"R2 trigger is pressed at value {event.value}")
+                        gantry = int((event.value + 1) * 64 + 127) #pwm 127 -> 255
 
             if event.type == pygame.JOYBUTTONDOWN:
 
@@ -74,24 +96,96 @@ try:
 
                 button_states[button_pressed] = True  # Set the button state to pressed
 
-                print(f"Button {button_pressed} is pressed")
+                if button_pressed == BUTTON_L1: #L1
+                    if spin > 10:
+                        spin = spin - 10
+
+                elif button_pressed == BUTTON_R1: #R1
+                    if spin < 245:
+                        spin = spin +10
+
+                if button_pressed == D_UP:
+                    if not isReverseCam1y:
+                        cam1y += 5
+                        if cam1y >= 180:
+                            isReverseCam1y = True
+                    else:
+                        cam1y -= 5
+                        if cam1y <= 0:
+                            isReverseCam1y = False
+
+                elif button_pressed == D_DOWN:
+                    if not isReverseCam2y:
+                        cam2y += 5
+                        if cam2y >= 180:
+                            isReverseCam2y = True
+                    else:
+                        cam2y -= 5
+                        if cam2y <= 0:
+                            isReverseCam2y = False
+
+                elif button_pressed == D_RIGHT:
+                    if not isReverseCam1x:
+                        cam1x += 5
+                        if cam1x >= 180:
+                            isReverseCam1x = True
+                    else:
+                        cam1x -= 5
+                        if cam1x <= 0:
+                            isReverseCam1x = False
+
+                elif button_pressed == D_LEFT:
+                    if not isReverseCam2x:
+                        cam2x += 5
+                        if cam2x >= 180:
+                            isReverseCam2x = True
+                    else:
+                        cam2x -= 5
+                        if cam2x <= 0:
+                            isReverseCam2x = False
 
 
-            elif event.type == pygame.JOYBUTTONUP:
+                elif button_pressed == BUTTON_OPTIONS:
+                    if not isReverseOptions:
+                        wrist_right += 5
+                        if wrist_right >= 255:
+                            isReverseOptions = True
+                    else:
+                        wrist_right -= 5
+                        if wrist_right <= 0:
+                            isReverseOptions = False
 
-                button_released = event.button
+                elif button_pressed == BUTTON_SHARE:
+                    if not isReverseShare:
+                        wrist_left += 5
+                        if wrist_left >= 255:
+                            isReverseShare = True
+                    else:
+                        wrist_left -= 5
+                        if wrist_left <= 0:
+                            isReverseShare = False
 
-                button_states[button_released] = False  # Set the button state to released
+                elif button_pressed == BUTTON_X:
+                    if 0 < shoulder <= 255:
+                        shoulder -= 5
 
-                print(f"Button {button_released} is released")
+                elif button_pressed == BUTTON_TRIANGLE:
+                    if 0 <= shoulder < 255:
+                        shoulder += 5
 
-                # Check button states continuously
+                elif button_pressed == BUTTON_O:
+                    if not isReverseClaw:
+                        claw += 5
+                        if claw >= 255:
+                            isReverseClaw = True
+                    else:
+                        claw -= 5
+                        if claw <= 0:
+                            isReverseClaw = False
 
-            for button_index, is_pressed in enumerate(button_states):
-
-                if is_pressed:
-                    print(f"Button {button_index} is being held down")
-
+            # Create and send Arm Command packet
+            arm_command = f"A_{shoulder}_{wrist_right}_{wrist_left}_{claw}_{gantry}_{spin}_{cam1x}_{cam1y}_{cam2x}_{cam2y}"
+            client_socket.send(arm_command.encode())
 
 except KeyboardInterrupt:
     # close the socket
--- RoverControl/serverCode.py
+++ RoverControl/serverCode.py
@@ -6,10 +6,82 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Bind the socket to a specific address and port
 server_socket.bind(("localhost", 12345))
 
+
+left_wheel1 = 0
+left_wheel2 = 0
+left_wheel3 = 0
+right_wheel1 = 0
+right_wheel2 = 0
+right_wheel3 = 0
+
+cam1x = 90
+cam1y = 90
+cam2x = 90
+cam2y = 90
+
+shoulder = 0
+wrist_right = 0
+wrist_left = 0
+claw = 0
+gantry = 0
+spin = 0
+
 print("Waiting for data...")
 
 while True:
     # Receive data
     data, client_address = server_socket.recvfrom(1024)
-    print(f"Received: {data.decode()}")
+    dataRecv = data.decode()
 
+    data_array = dataRecv.split("_")
+
+    if data_array[0] == "D":
+
+        left_wheel1 = data_array[1]
+        left_wheel2 = data_array[2]
+        left_wheel3 = data_array[3]
+        right_wheel1 = data_array[4]
+        right_wheel2 = data_array[5]
+        right_wheel3 = data_array[6]
+
+        print("{"
+              "Drive_Command:\n"
+              "\tleft_wheel1: " + left_wheel1 + "\n"
+              "\tleft_wheel2: " + left_wheel2 + "\n"
+              "\tleft_wheel3: " + left_wheel3 + "\n"
+              "\tright_wheel1: " + right_wheel1 + "\n"
+              "\tright_wheel2: " + right_wheel2 + "\n"
+              "\tright_wheel3: " + right_wheel3 + "\n"
+              "}")
+
+    if data_array[0] == "A":
+
+        shoulder = data_array[1]
+        wrist_right = data_array[2]
+        wrist_left = data_array[3]
+        claw = data_array[4]
+        gantry = data_array[5]
+        spin = data_array[6]
+
+        cam1x = data_array[6]
+        cam1y = data_array[7]
+        cam2x = data_array[8]
+        cam2y = data_array[9]
+
+        print("{"
+              "Arm_Command:\n"
+              "\tshoulder: " + shoulder + "\n"
+              "\twrist_right: " + wrist_right + "\n"
+              "\twrist_left: " + wrist_left + "\n"
+              "\tclaw: " + claw + "\n"
+              "\tgantry: " + gantry + "\n"
+              "\tspin: " + spin + "\n"
+              "}")
+
+        print("{"
+              "Camera_Position\n"
+              "\tcam1x: " + cam1x + "\n"
+              "\tcam1y: " + cam1y + "\n"
+              "\tcam2x: " + cam2x + "\n"
+              "\tcam2y: " + cam2y + "\n"
+              "}")
